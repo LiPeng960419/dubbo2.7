666e2378aa72ae76bf705ec9fec8c62356d8b83b
LiPeng960419 <lp960419@163.com>
2021-04-30 00:26:40 +0800

    todo dubbo apollo启动之后 dubbo配置不生效

--- provider-demo/src/main/java/com/lipeng/providerdemo/ProviderDemoApplication.java
+++ provider-demo/src/main/java/com/lipeng/providerdemo/ProviderDemoApplication.java
@@ -1,17 +1,34 @@
 package com.lipeng.providerdemo;
 
+import com.lipeng.common.utils.SpringContextUtils;
+import com.lipeng.common.vo.ResultVo;
+import java.util.Map;
+import org.apache.dubbo.config.AbstractConfig;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDubbo
 @ComponentScan({"com.lipeng.common", "com.lipeng.providerdemo"})
+@RestController
 public class ProviderDemoApplication {
 
 	public static void main(String[] args) {
 		new SpringApplication(ProviderDemoApplication.class).run(args);
+	}
+
+	@Value("${app.name:error}")
+	private String name;
+
+	@GetMapping
+	public ResultVo resultVo() {
+		Map<String, AbstractConfig> beansOfType = SpringContextUtils.getBeansByClass(AbstractConfig.class);
+		return ResultVo.success(name);
 	}
 
 }
